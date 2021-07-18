a8de70bc20ac0ffa0e5d2c148117ed728fac2157
Piyush Kumar <iyu1kor@bosch.com>
2021-07-18 19:46:49 +0530

    Added 1st Question and Solution for Queue: First Non Repeating Letter Problem.

---
+++ 9. Queue/1.first-non-repeating-letter-solution.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Queue []interface{}
+
+func (q *Queue) isEmpty() bool {
+	return len(*q) == 0
+}
+
+func (q *Queue) push(item interface{}) {
+	*q = append(*q, item)
+}
+
+func (q *Queue) pop() (interface{}, bool) {
+	if q.isEmpty() {
+		return "", false
+	} else {
+		*q = (*q)[1:]
+		return (*q)[0], true
+	}
+}
+
+func (q *Queue) popAtIndex(index int) bool {
+	if q.isEmpty() {
+		return false
+	} else {
+		*q = append((*q)[:index], (*q)[index+1:]...)
+		return true
+	}
+}
+
+func (q *Queue) peek() (interface{}, bool) {
+	if q.isEmpty() {
+		return "", false
+	} else {
+		return (*q)[0], true
+	}
+}
+
+func linearSearch(arr []interface{}, item string) int {
+	for i, v := range arr {
+		if item == v {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func firstNonRepeatingLetter(str string) []string {
+	var q Queue
+	var res []string
+	if len(str) == 0 {
+		return res
+	}
+	m := make(map[string]int)
+
+	for _, v := range str {
+		_, ok := m[string(v)]
+		if ok {
+			index := linearSearch(q, string(v))
+			if index != -1 {
+				q.popAtIndex(index)
+			}
+			if q.isEmpty() {
+				res = append(res, "-1")
+
+			} else {
+				ele, _ := q.peek()
+				res = append(res, ele.(string))
+			}
+
+		} else {
+			m[string(v)] = 1
+			if q.isEmpty() {
+				res = append(res, string(v))
+
+			} else {
+				ele, _ := q.peek()
+				res = append(res, ele.(string))
+			}
+			q.push(string(v))
+		}
+	}
+
+	return res
+}
+
+func main() {
+	var str string
+	fmt.Print("Enter the string ")
+	_, _ = fmt.Scan(&str)
+
+	result := firstNonRepeatingLetter(str)
+
+	for _, v := range result {
+		fmt.Printf("%s ", string(v))
+	}
+
+}
---
+++ 9. Queue/README.md
@@ -0,0 +1,12 @@
+# Queue
+1. First Non Repeating Letter Problem.
+   <br /> [Question](/9.%20Queue/docs/1.first-non-repeating-letter-question.jpg)
+   <br /> [Solution](/9.%20Queue/1.first-non-repeating-letter-solution.go)
+   <br /> Command:-
+   ```shell
+   $ go run 1.first-non-repeating-letter-solution.go
+   ```
+
+   | Method | TimeComplexity | SpaceComplexity | Source |
+   |---|---|---|---|
+   | Map and Queue | O(N) | O(N) | [Link](/9.%20Queue/1.first-non-repeating-letter-solution.go) |
